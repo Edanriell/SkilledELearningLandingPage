9128c72a5ae1f57cfab35b50a2b41a166d1edaca
Edanriell <Edanriell@gmail.com>
2024-06-09 17:56:59 +0300

    started work on hero section

--- Client/pages/main-page/sections/hero/ui.vue
+++ Client/pages/main-page/sections/hero/ui.vue
@@ -1,7 +1,38 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+	import { Button } from "@shared/ui";
+</script>
 
 <template>
 	<section>
-		<h2>Test</h2>
+		<div>
+			<h2>Maximize skill, minimize budget</h2>
+			<p>
+				Our modern courses across a range of in-demand skills will give you the knowledge you need
+				to live the life you want.
+			</p>
+			<Button background-color="gradientLight">Get Started</Button>
+		</div>
+		<div>
+			<div>
+				<picture>
+					<source
+						media="(min-width: 1440px)"
+						srcset="/images/woman-image-lg.png"
+						type="image/png"
+					/>
+					<source media="(min-width: 768px)" srcset="/images/woman-image-md.png" type="image/png" />
+					<source media="(min-width: 375px)" srcset="/images/woman-image-sm.png" type="image/png" />
+					<img alt="Image of a woman drinking coffee" src="/images/woman-image-sm.png" />
+				</picture>
+			</div>
+			<dl>
+				<dt>Members</dt>
+				<dd>29k</dd>
+			</dl>
+			<dl>
+				<dt>Course hours</dt>
+				<dd>1,451</dd>
+			</dl>
+		</div>
 	</section>
 </template>
