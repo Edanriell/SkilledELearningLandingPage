1b8b98ea9e83368a2c014367219f5632b85de69b
Edanriell <Edanriell@gmail.com>
2024-06-09 22:26:25 +0300

    created mobile version of hero

---
+++ Client/pages/main-page/sections/hero/styles.ts
@@ -0,0 +1,106 @@
+import { css } from "@styled-system/css";
+
+export const hero = css({
+	backgroundColor: "var(--ffffff)",
+	paddingTop: "2.2rem",
+	paddingBottom: "6.6rem"
+});
+
+export const heroContent = css({
+	backgroundColor: "var(--ffffff)",
+	paddingLeft: "1.6rem",
+	paddingRight: "1.6rem",
+	marginBottom: "7rem"
+});
+
+export const heroTitle = css({
+	fontFamily: "var(--font-family)",
+	fontWeight: "800",
+	fontSize: "4rem",
+	color: "var(--13183f)",
+	marginBottom: "2.6rem"
+});
+
+export const heroText = css({
+	fontFamily: "var(--font-family)",
+	fontWeight: "500",
+	fontSize: "1.6rem",
+	lineHeight: "162%",
+	color: "var(--83869a)",
+	marginBottom: "2.4rem"
+});
+
+export const heroButtonContainer = css({
+	textAlign: "left"
+});
+
+export const heroStatistics = css({
+	position: "relative"
+});
+
+export const heroImageContainer = css({
+	position: "relative",
+	display: "block",
+	margin: "0 auto",
+	width: "25.3rem",
+	height: "25.3rem"
+});
+
+export const heroImage = css({
+	width: "25.3rem",
+	height: "25.3rem",
+	borderRadius: "15.8rem",
+	background: "linear-gradient(180deg, #ff6f48 0%, #f02aa6 100%)",
+	objectFit: "cover",
+	overflow: "hidden",
+	textAlign: "center"
+});
+
+export const statistics = css({
+	padding: "1.6rem 2.1rem ",
+	display: "flex",
+	flexDirection: "column",
+	borderRadius: "1rem",
+	boxShadow: "0 34px 54px 0 rgba(6, 22, 141, 0.16)",
+	background: "var(--ffffff)",
+	position: "absolute"
+});
+
+export const statisticsFieldName = css({
+	fontFamily: "var(--font-family)",
+	fontWeight: "700",
+	fontSize: "1.4rem",
+	lineHeight: "200%",
+	color: "var(--83869a)"
+});
+
+export const statisticsFieldValue = css({
+	fontFamily: "var(--font-family)",
+	fontWeight: "800",
+	fontSize: "3.2rem",
+	color: "var(--13183f)"
+});
+
+export const statisticsHours = css({
+	top: "17.7rem",
+	left: "-4.7rem"
+});
+
+export const statisticsMembers = css({
+	top: "-2.4rem",
+	right: "-4.7rem"
+});
+
+export const container = css({
+	padding: "1.6rem 1.6rem",
+	display: "flex",
+	flexDirection: "row",
+	alignItems: "center",
+	justifyContent: "space-between",
+	md: {
+		padding: "2.4rem 4rem"
+	},
+	lg: {
+		padding: "2.4rem 16.5rem"
+	}
+});
--- Client/pages/main-page/sections/hero/ui.vue
+++ Client/pages/main-page/sections/hero/ui.vue
@@ -1,19 +1,37 @@
 <script lang="ts" setup>
 	import { Button } from "@shared/ui";
+
+	import {
+		hero,
+		heroButtonContainer,
+		heroContent,
+		heroImage,
+		heroImageContainer,
+		heroStatistics,
+		heroText,
+		heroTitle,
+		statistics,
+		statisticsFieldName,
+		statisticsFieldValue,
+		statisticsHours,
+		statisticsMembers
+	} from "./styles";
 </script>
 
 <template>
-	<section>
-		<div>
-			<h2>Maximize skill, minimize budget</h2>
-			<p>
+	<section :class="hero">
+		<div :class="heroContent">
+			<h2 :class="heroTitle">Maximize skill, minimize budget</h2>
+			<p :class="heroText">
 				Our modern courses across a range of in-demand skills will give you the knowledge you need
 				to live the life you want.
 			</p>
-			<Button background-color="gradientLight">Get Started</Button>
+			<div :class="heroButtonContainer">
+				<Button background-color="gradientLight">Get Started</Button>
+			</div>
 		</div>
-		<div>
-			<div>
+		<div :class="heroStatistics">
+			<div :class="heroImageContainer">
 				<picture>
 					<source
 						media="(min-width: 1440px)"
@@ -22,17 +40,21 @@
 					/>
 					<source media="(min-width: 768px)" srcset="/images/woman-image-md.png" type="image/png" />
 					<source media="(min-width: 375px)" srcset="/images/woman-image-sm.png" type="image/png" />
-					<img alt="Image of a woman drinking coffee" src="/images/woman-image-sm.png" />
+					<img
+						:class="heroImage"
+						alt="Image of a woman drinking coffee"
+						src="/images/woman-image-sm.png"
+					/>
 				</picture>
+				<dl :class="statistics + ' ' + statisticsMembers">
+					<dt :class="statisticsFieldName">Members</dt>
+					<dd :class="statisticsFieldValue">29k</dd>
+				</dl>
+				<dl :class="statistics + ' ' + statisticsHours">
+					<dt :class="statisticsFieldName">Course hours</dt>
+					<dd :class="statisticsFieldValue">1,451</dd>
+				</dl>
 			</div>
-			<dl>
-				<dt>Members</dt>
-				<dd>29k</dd>
-			</dl>
-			<dl>
-				<dt>Course hours</dt>
-				<dd>1,451</dd>
-			</dl>
 		</div>
 	</section>
 </template>
