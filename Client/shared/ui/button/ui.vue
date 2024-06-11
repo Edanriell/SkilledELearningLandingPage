7f93cb298453416659a5a92eee5cb7b7bd0eed83
Edanriell <Edanriell@gmail.com>
2024-06-11 22:33:21 +0300

    almost finished button animations

--- Client/shared/ui/button/ui.vue
+++ Client/shared/ui/button/ui.vue
@@ -27,36 +27,143 @@
 	};
 
 	const handleMouseEnter = () => {
-		// gsap.set(buttonRef.value, {
-		// 	background: "#3d8168"
-		// });
-
 		gsap.to(buttonRef.value, {
 			duration: 0.25,
 			scale: 1.05,
 			ease: "power2"
 		});
+
+		if (props.backgroundColor === "dark") {
+			gsap.set(buttonRef.value, {
+				background: "#13183f"
+			});
+
+			gsap.to(buttonRef.value, {
+				duration: 0.25,
+				background: "#666ca3",
+				ease: "power2"
+			});
+		} else if (props.backgroundColor === "gradientLight") {
+			gsap.set(buttonRef.value, {
+				background: "linear-gradient(180deg, #ff6f48 0%, #f02aa6 100%)"
+			});
+
+			gsap.to(buttonRef.value, {
+				duration: 0.25,
+				background: "linear-gradient(180deg, #ff6f48 0%, #f02aa6 100%)",
+				opacity: 0.5,
+				ease: "power2"
+			});
+		} else if (props.backgroundColor === "gradientDark") {
+			gsap.set(buttonRef.value, {
+				background: "linear-gradient(180deg, #4851ff 0%, #f02aa6 100%)"
+			});
+
+			gsap.to(buttonRef.value, {
+				duration: 0.25,
+				background: "linear-gradient(180deg, #4851ff 0%, #f02aa6 100%)",
+				opacity: 0.5,
+				ease: "power2"
+			});
+		} else if (props.backgroundColor === "basic") {
+			gsap.set(buttonRef.value, {
+				color: "#f74780"
+			});
+
+			gsap.to(buttonRef.value, {
+				duration: 0.25,
+				color: "#ffa7c3",
+				ease: "power2"
+			});
+		}
 	};
 
 	const handleMouseLeave = () => {
 		gsap.to(buttonRef.value, {
-			// background: "#3d8168",
 			duration: 0.25,
 			scale: 1,
 			ease: "power2"
 		});
+
+		if (props.backgroundColor === "dark") {
+			gsap.to(buttonRef.value, {
+				duration: 0.25,
+				background: "#13183f",
+				ease: "power2"
+			});
+		} else if (props.backgroundColor === "gradientLight") {
+			gsap.to(buttonRef.value, {
+				duration: 0.25,
+				background: "linear-gradient(180deg, #ff6f48 0%, #f02aa6 100%)",
+				opacity: 1,
+				ease: "power2"
+			});
+		} else if (props.backgroundColor === "gradientDark") {
+			gsap.to(buttonRef.value, {
+				duration: 0.25,
+				background: "linear-gradient(180deg, #4851ff 0%, #f02aa6 100%)",
+				opacity: 1,
+				ease: "power2"
+			});
+		} else if (props.backgroundColor === "basic") {
+			gsap.to(buttonRef.value, {
+				duration: 0.25,
+				color: "#f74780",
+				ease: "power2"
+			});
+		}
 	};
 
 	const handleTouchStart = () => {
-		// gsap.set(buttonRef.value, {
-		// 	background: "#3d8168"
-		// });
-
 		gsap.to(buttonRef.value, {
 			duration: 0.25,
 			scale: 0.95,
 			ease: "power2"
 		});
+
+		if (props.backgroundColor === "dark") {
+			gsap.set(buttonRef.value, {
+				background: "#13183f"
+			});
+
+			gsap.to(buttonRef.value, {
+				duration: 0.25,
+				background: "#666ca3",
+				ease: "power2"
+			});
+		} else if (props.backgroundColor === "gradientLight") {
+			gsap.set(buttonRef.value, {
+				background: "linear-gradient(180deg, #ff6f48 0%, #f02aa6 100%)"
+			});
+
+			gsap.to(buttonRef.value, {
+				duration: 0.25,
+				background: "linear-gradient(180deg, #ff6f48 0%, #f02aa6 100%)",
+				opacity: 0.5,
+				ease: "power2"
+			});
+		} else if (props.backgroundColor === "gradientDark") {
+			gsap.set(buttonRef.value, {
+				background: "linear-gradient(180deg, #4851ff 0%, #f02aa6 100%)"
+			});
+
+			gsap.to(buttonRef.value, {
+				duration: 0.25,
+				background: "linear-gradient(180deg, #4851ff 0%, #f02aa6 100%)",
+				opacity: 0.5,
+				ease: "power2"
+			});
+		} else if (props.backgroundColor === "basic") {
+			gsap.set(buttonRef.value, {
+				color: "#f74780"
+			});
+
+			gsap.to(buttonRef.value, {
+				duration: 0.25,
+				color: "#ffa7c3",
+				ease: "power2"
+			});
+		}
 	};
 
 	const handleTouchEnd = () => {
@@ -65,6 +172,34 @@
 			scale: 1,
 			ease: "power2"
 		});
+
+		if (props.backgroundColor === "dark") {
+			gsap.to(buttonRef.value, {
+				duration: 0.25,
+				background: "#13183f",
+				ease: "power2"
+			});
+		} else if (props.backgroundColor === "gradientLight") {
+			gsap.to(buttonRef.value, {
+				duration: 0.25,
+				opacity: 1,
+				background: "linear-gradient(180deg, #ff6f48 0%, #f02aa6 100%)",
+				ease: "power2"
+			});
+		} else if (props.backgroundColor === "gradientDark") {
+			gsap.to(buttonRef.value, {
+				duration: 0.25,
+				background: "linear-gradient(180deg, #4851ff 0%, #f02aa6 100%)",
+				opacity: 1,
+				ease: "power2"
+			});
+		} else if (props.backgroundColor === "basic") {
+			gsap.to(buttonRef.value, {
+				duration: 0.25,
+				color: "#f74780",
+				ease: "power2"
+			});
+		}
 	};
 </script>
 
